bc13d72b0234025da8736f1a9980ea5007dbc4b9
ybKim <ybkim@codebrick.co>
2022-01-24 10:06:13 +0900

    study

---
+++ src/basic/func.go
@@ -0,0 +1,69 @@
+package main
+
+import "fmt"
+
+type Pointer struct {
+	var1 string
+	var2 int
+}
+
+func main() {
+	msg := "Hello"
+	say(msg)
+
+	msg2 := 12345
+	say2(&msg2)
+
+	pointer := Pointer{"pointer test", 100}
+	say3(&pointer)
+
+	total := sum(1, 2, 3, 4, 5)
+	println("total = ", total)
+
+	count2, total2 := sum2(1, 2, 3, 4, 5)
+	fmt.Printf("count2=%v, total2=%v\n", count2, total2)
+
+	count3, total3 := sum2(1, 2, 3, 4, 5)
+	fmt.Printf("count3=%v, total3=%v\n", count3, total3)
+}
+
+func say(msg string) {
+	println(msg)
+}
+
+func say2(msg *int) {
+	//println(*msg)
+	fmt.Printf("%v, %v, %v, %v, %v\n", msg, *msg, &msg, *&msg, **&msg)
+	//*msg = "Changed"
+}
+
+func say3(pointer *Pointer) {
+	fmt.Printf("%v, %v, %v, %v, %v\n", pointer, *pointer, &pointer, pointer.var1, pointer.var2)
+}
+
+func sum(nums ...int) int {
+	s := 0
+	for _, n := range nums {
+		s += n
+	}
+	return s
+}
+
+func sum2(nums ...int) (int, int) {
+	s := 0
+	count := 0
+	for _, n := range nums {
+		s += n
+		count++
+	}
+	//count = len(nums)
+	return count, s
+}
+
+func sum3(nums ...int) (count int, total int) {
+	for _, n := range nums {
+		total += n
+	}
+	count = len(nums)
+	return
+}
---
+++ src/basic/iterate.go
@@ -0,0 +1,61 @@
+package main
+
+func main() {
+	sum := 0
+	for i := 1; i <= 100; i++ {
+		sum += i
+	}
+	println("sum= ", sum)
+
+	n := 1
+	for n < 100 { //for ;n < 100; {
+		n *= 2
+		println("n=", n)
+	}
+
+	// for {
+	// 	time.Sleep(time.Millisecond * 500)
+	// 	fmt.Printf("Infinite loop, %v\n", time.Now())
+	// }
+	// 무한루프
+
+	//range
+	names := []string{"홍길동", "이순신", "강감찬"}
+	for i, name := range names {
+		println(i, name)
+	}
+
+	//goto
+	var a = 1
+	for a < 15 {
+		if a == 5 {
+			a += a
+			continue
+		}
+		a++
+		if a > 10 {
+			break
+		}
+	}
+	if a == 11 {
+		goto END
+	}
+	a += 100
+
+END:
+	println("a = ", a)
+	println("End")
+
+	//break label
+	i := 0
+
+L1:
+	for {
+		i++
+		if i == 10 {
+			break L1 //L1 바로 밑에있는 for 루프를 빠져 나간다는 뜻
+		}
+		println("i <= ", i)
+	}
+	println("OK == ", i)
+}
